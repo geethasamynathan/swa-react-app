be968bd40ae2beab3059c103a18934a91a04c822
geethasamynathan <geethasamynathan2011@gmail.com>
2025-02-22 07:48:51 +0530

    swa-sample -app added

---
+++ routedemo/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h2> Welocme to Our online Store </h2>
+      <p> Find your favourite needs here</p>
+    </div>
+  );
+}
